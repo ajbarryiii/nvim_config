71df94236465b9adeb74a597ee9c89a6c0e8edcc
ajbarryiii <ajbarryiii@gmail.com>
2024-11-12 12:55:28 -0800

    initial commit

---
+++ init.lua
@@ -0,0 +1,268 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Set leader key to space
+vim.g.mapleader = " "
+
+-- Key mappings
+-- Open file explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move selected text down or up in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Join lines without moving the cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Center the cursor after scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Center after searching next or previous match
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste without overwriting register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Yank to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete without overwriting register
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- Map <C-c> as an escape alternative
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Disable Ex mode
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Open tmux sessionizer
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- Format code using LSP
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Navigate quickfix and location lists with centering
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Make file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Open Neovim configuration file
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
+
+-- Run cellular automaton
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
+
+-- Reload Neovim configuration
+vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
+
+-- Key remappings for normal and visual modes (swap 'j', 'k', 'l' with 't', 'n', 's')
+vim.api.nvim_set_keymap('n', 'n', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 't', 'j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 's', 'l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'n', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 't', 'j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 's', 'l', { noremap = true, silent = true })
+
+-- Performance and appearance settings
+vim.opt.relativenumber = true
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.redrawtime = 1500
+vim.opt.hidden = true
+vim.o.completeopt = 'menuone,noselect'
+
+
+
+-- Install and configure plugins using lazy.nvim
+require("lazy").setup({
+    -- LSP Configurations for Neovim
+    { "neovim/nvim-lspconfig" },
+	-- Null-ls for additional formatting and linting options
+    { "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    
+    -- Autocompletion plugins
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-vsnip",
+            "hrsh7th/vim-vsnip"
+        },
+        config = function()
+            require('cmp').setup({
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                },
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                mapping = {
+                    ['<C-Space>'] = require('cmp').mapping.complete(),
+                    ['<CR>'] = require('cmp').mapping.confirm({ select = true }),
+                    ['<Tab>'] = require('cmp').mapping(function(fallback)
+                        if require('cmp').visible() then
+                            require('cmp').select_next_item()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                },
+                window = {
+                    completion = { border = 'rounded', max_height = 20, max_width = 50 }
+                }
+            })
+        end,
+    },
+
+    {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+	  }
+     },
+
+    
+    -- Nvim-tree file explorer with file icons
+    {
+        "kyazdani42/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require'nvim-tree'.setup {}
+            vim.api.nvim_set_keymap('n', '<leader>th', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+        end,
+    },
+    
+    -- Treesitter for syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = { "cpp", "python", "lua" },
+                highlight = {
+                    enable = true,
+                },
+            }
+        end,
+    },
+    
+    -- Lspsaga for LSP UI enhancements
+    {
+        "glepnir/lspsaga.nvim",
+        branch = "main",
+        config = function()
+            require('lspsaga').setup {}
+        end,
+    },
+    
+    -- Linting plugin
+    {
+        "mfussenegger/nvim-lint",
+    },
+
+    {
+        "bluz71/vim-moonfly-colors",
+        lazy = true,
+    },
+    {
+	 "scottmckendry/cyberdream.nvim",
+	 lazy = false,
+	 priority = 1000,
+    },
+
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme catppuccin")
+        end,
+    },
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+})
+
+require("lsp")
