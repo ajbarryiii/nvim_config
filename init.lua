ec5b7cd3933a51d4ab5c806b0fa71899a1369b07
ajbarryiii <ajbarryiii@gmail.com>
2024-11-14 09:36:08 -0800

    updating lsp

--- init.lua
+++ init.lua
@@ -72,9 +72,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- Make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- Open Neovim configuration file
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
-
 -- Run cellular automaton
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 
@@ -265,4 +262,4 @@ require("lazy").setup({
     },
 })
 
-require("lsp")
+require("lsp_test")
