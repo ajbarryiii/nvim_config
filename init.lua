76457070b020560ac455f2a3ad2fb98169d88e5f
ajbarryiii <ajbarryiii@gmail.com>
2024-12-18 09:41:48 -0800

    todo-comments

--- init.lua
+++ init.lua
@@ -252,6 +252,17 @@ require("lazy").setup({
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
     },
+    -- todo comments
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    }
+
     --
     -- Trouble
     {
