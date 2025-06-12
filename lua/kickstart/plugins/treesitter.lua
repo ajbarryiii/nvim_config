776d67e5b7ef4e5f52313031beaff45d69667d3d
ajbarryiii <ajbarryiii@gmail.com>
2025-06-12 15:38:22 -0700

    adding support for mojo in treesitter config

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -35,6 +35,12 @@ return {
 			},
 			indent = { enable = true, disable = { "ruby" } },
 		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+
+			-- Use Python parser for Mojo files
+			vim.treesitter.language.register("python", "mojo")
+		end,
 		-- There are additional nvim-treesitter modules that you can use to interact
 		-- with nvim-treesitter. You should go explore a few and see what interests you:
 		--
