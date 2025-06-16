b9ded1946961f232ea2c2e527d07c9c800c66476
ajbarryiii <ajbarryiii@gmail.com>
2025-06-16 12:59:15 -0700

    revert most recent change

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -49,7 +49,6 @@ require("lazy").setup({
 
 	require("kickstart/plugins/supermaven"),
 
-	require("kickstart/plugins/ocp-indent"),
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
